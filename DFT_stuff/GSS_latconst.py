358547f184440ccdb34586d6f3362939b56bfe70
Kevin Krempl <kevinkrempl@gmail.com>
2018-11-18 15:12:06 -0800

    added slurm header

--- DFT_stuff/GSS_latconst.py
+++ DFT_stuff/GSS_latconst.py
@@ -1,3 +1,41 @@
+#/usr/bin/env python
+#| - SLURM HEADER
+#above line selects special python interpreter which knows all the paths
+#SBATCH -p iric,owners
+#################
+#set a job name
+#SBATCH --job-name=recon1
+#################
+#a file for job output, you can check job progress
+#SBATCH --output=myjob.out
+#################
+# a file for errors from the job
+#SBATCH --error=myjob.err
+#################
+#time you think you need; default is one hour
+#in minutes in this case
+#SBATCH --time=00:30:00
+#################
+#quality of service; think of it as job priority
+#SBATCH --qos=normal
+#################
+#number of nodes you are requesting
+#SBATCH --nodes=1
+#################
+#memory per node; default is 4000 MB per CPU
+#SBATCH --mem-per-cpu=4000
+#you could use --mem-per-cpu; they mean what we are calling cores
+#################
+#get emailed about job BEGIN, END, and FAIL
+#SBATCH --mail-type=ALL
+#################
+#who to send email to; please change to your email
+#SBATCH  --mail-user=kkrempl@stanford.edu
+#################
+#task to run per node; each node has 16 cores
+#SBATCH --ntasks-per-node=16
+#################
+#__|
 """Finding dft-calculated lattice constants with golden section search
 """
 
